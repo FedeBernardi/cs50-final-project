402a184ea48a92b36462979af63dbdec532c781f
Federico Bernardi <fedeew94@hotmail.com.ar>
2018-12-01 17:45:49 -0300

    Radio Buttons and Textinput components

---
+++ src/components/formComponents/CustomTextInput.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {TextInput, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class CustomTextInput extends React.Component {
+
+    static propTypes = {
+        onChangeText: PropTypes.func.isRequired,
+        placeholder: PropTypes.string,
+        keyboardType: PropTypes.string
+    }
+
+    render() {
+        return <TextInput
+            style={styles.input}
+            onChangeText={this.onChangeName}
+            placeholder={this.props.placeholder}
+            keyboardType={this.props.keyboardType ? this.props.keyboardType : 'default'}
+            underlineColorAndroid='transparent'
+        />
+    }
+
+}
+
+const styles = StyleSheet.create({
+    input: {
+        marginBottom: 10,
+        width: '100%',
+        height: 40,
+        paddingLeft: 5,
+        borderWidth: 1,
+        backgroundColor: '#FFF'
+    }
+});
---
+++ src/components/formComponents/RadioButtonsGroup.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class RadioButtonsGroup extends React.Component {
+
+    static propTypes = {
+        options: PropTypes.array.isRequired,
+        onSelection: PropTypes.func.isRequired,
+        isVertical: PropTypes.bool
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedOption: -1
+        }
+        
+        this.onSelection = this.onSelection.bind(this);
+    }
+
+    onSelection(optionIndex, value) {
+        this.setState({selectedOption: optionIndex});
+        this.props.onSelection(value);
+    }
+
+    render() {
+        const {isVertical, options} = this.props;
+
+        return <View style={isVertical ? {} : styles.horizontalContainer}>
+            {options.map(({label, value}, index) => <TouchableOpacity
+                onPress={() => this.onSelection(index, value)}
+                style={[styles.optionContainer, isVertical ? styles.verticalOption : styles.horizontalOption]}
+                key={index}
+            >
+                <View style={styles.circle}>
+                    <View style={[styles.innerCircle, this.state.selectedOption === index ? styles.selected : {}]}></View>
+                </View>
+                <Text style={styles.label}>{label}</Text>
+            </TouchableOpacity>)}
+        </View>;
+    }
+}
+
+const styles = StyleSheet.create({
+    horizontalContainer: {
+        flexDirection: 'row'
+    },
+    verticalOption: {
+        marginBottom: 20
+    },
+    horizontalOption: {
+        marginRight: 20
+    },
+    optionContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    circle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: '#FFF',
+        borderColor: '#FF637D',
+        borderWidth: 4,
+        padding: 20,
+        marginRight: 10
+    },
+    innerCircle: {
+        width: 30,
+        height: 30,
+        borderRadius: 20
+    },
+    label: {
+        fontSize: 20
+    },
+    selected: {
+        backgroundColor: '#FF637D'
+    }
+});
