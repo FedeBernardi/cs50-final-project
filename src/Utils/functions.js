7e201e328d77ab5f0011729a0e5ba05e1ac11050
Federico Bernardi <fedeew94@hotmail.com.ar>
2018-12-09 22:27:49 -0300

    Function to format dates

--- src/Utils/functions.js
+++ src/Utils/functions.js
@@ -5,3 +5,15 @@ export function mergeObjects(baseObj, obj) {
 export function isEmpty(obj) {
     return Object.keys(obj).length === 0;
 }
+
+/**
+ * It receives a string, converts it to a Date object and
+ * returns the formatted date.
+ *
+ * @param {String} date
+ */
+export function getDateString(date) {
+    const dateObj = new Date(date);
+
+    return dateObj.toLocaleDateString('en-US');
+}
